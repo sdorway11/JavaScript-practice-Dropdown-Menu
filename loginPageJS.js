77bfebe1c56ef7c146da3b4b311175a73bd7d376
Spencer <sdorway11@gmail.com>
2016-02-22 18:58:35 -0500

    Update loginPageJS.js
    adding comments

--- loginPageJS.js
+++ loginPageJS.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     //hide the p tags
     $("li").hide();
 
-    //Click to show the p tag
+    //hover to show the menu
     $(".dropdown").mouseenter(function () {
         $("li").show(500);
         $(".dropbtn").css({
@@ -10,6 +10,7 @@ $(document).ready(function () {
             "border-radius": "25px 25px 0px 0px"
         });
     });
+    //remove hover to return to first state
     $(".dropdown").mouseleave(function () {
         $("li").hide(500);
         $(".dropbtn").css({
@@ -17,11 +18,13 @@ $(document).ready(function () {
             "border-radius": "25px"
         });
     });
+    //selecting a menu item by hovering
     $("li").mouseenter(function () {
         $(this).css({
             "background-color": "initial",
         });
     });
+    //return state after hovering
     $("li").mouseleave(function () {
         $(this).css({
             "background-color": "#0ca3d2",
